5825187e952b0ed1f4fd716d6f6ea9d4055354d0
Taisa <taisa.7591@yahoo.com>
2021-05-05 20:22:32 +0300

    mod

--- modul3_2/app1.py
+++ modul3_2/app1.py
@@ -1,4 +1,4 @@
-
+# result : [['Section1', value1, value2], ['Section2', value1, value2], ...]
 
 data = """
 _____
@@ -16,4 +16,35 @@ Section3
 _____
 value1
 value2
+
 """
+
+def parse_data(data:str):
+    my_section = False
+    linii = data.splitlines()
+    result = []
+    print(linii)
+    for line in linii:
+        if line=="":
+            continue
+        if "_____" == line and my_section is False:
+            my_section = True
+            print("New section")
+        elif "_____" == line and my_section is True:
+            my_section = False
+            print("End new section")
+        elif my_section:
+            print("Section name is", line)
+            result.append([line])
+        elif not my_section:
+            result[-1].append(line)
+
+        print(line)
+    return result
+var = parse_data(data)
+print(var)
+
+
+
+
+
---
+++ modul3_2/app2.py
@@ -0,0 +1,20 @@
+# result : [['Section1', value1, value2], ['Section2', value1, value2], ...]
+
+data = """
+_____
+Section1
+_____
+value1
+value2
+_____
+Section2
+_____
+value1
+value2
+_____
+Section3
+_____
+value1
+value2
+
+"""
